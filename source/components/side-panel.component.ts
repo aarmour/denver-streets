4785cd986afa2f664049357741ba3640be0c89ec
aarmour <adam.armour@gmail.com>
2016-01-24 09:13:54 -0700

    feat(side-panel): add toggle tab

--- source/components/side-panel.component.ts
+++ source/components/side-panel.component.ts
@@ -15,7 +15,6 @@ import { Component, EventEmitter } from 'angular2/core';
         width: 22em;
         height: 100%;
         padding: 1em;
-        overflow-y: scroll;
         z-index: 1;
         background-color: #fff;
         box-shadow: 1px 0px 2px rgba(0,0,0,0.3);
@@ -24,6 +23,11 @@ import { Component, EventEmitter } from 'angular2/core';
 
       .side-panel.open {
         left: 0;
+      }
+
+      .content {
+        height: inherit;
+        overflow-y: scroll;
       }
 
       .btn-close {
@@ -45,20 +49,51 @@ import { Component, EventEmitter } from 'angular2/core';
         font-size: 2em;
         line-height: 0;
       }
+
+      .toggle-tab {
+        position: absolute;
+        top: 8px;
+        left: 100%;
+      }
+
+      .toggle-tab button {
+        height: 42px;
+        color: #A5A5A5;
+        background: #fff;
+        border: solid 1px #ddd;
+        outline: 0;
+      }
+
+      .toggle-tab button:focus {
+        background: #f3f3f3;
+      }
     </style>
 
     <div class="side-panel" [ngClass]="{open: isOpen}">
       <button class="btn-close" *ngIf="showCloseBtn" (click)="close()"></button>
-      <ng-content></ng-content>
+      <div class="toggle-tab" *ngIf="showToggleTab">
+        <button (click)="toggle()">
+          <span *ngIf="isOpen">≪</span>
+          <span *ngIf="!isOpen">≫</span>
+        </button>
+      </div>
+      <div class="content">
+        <ng-content></ng-content>
+      </div>
     </div>
   `
 })
 export default class SidePanel {
 
   public isOpen: boolean = false;
-  public showCloseBtn: boolean = true;
+  public showCloseBtn: boolean = false;
+  public showToggleTab: boolean = true;
 
   close() {
     this.isOpen = false;
+  }
+
+  toggle() {
+    this.isOpen = !this.isOpen;
   }
 }
