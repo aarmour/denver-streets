9ebc10f6406abaf5f68f7d5c1b85aeabf776cd78
Adam Armour <adam.armour@teamaol.com>
2016-02-13 18:29:57 -0700

    refactor: add Home container for primary view

--- src/common/containers/App.js
+++ src/common/containers/App.js
@@ -1,44 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import Panel from '../components/Panel';
-import MapGL from '../components/MapGL';
-import IndexPanel from './IndexPanel';
 
 class App extends Component {
 
   get styles() {
     return {
-      root: { height: '100%' },
-      content: { height: '100%' }
+      root: { height: '100%' }
+
     };
-  }
-
-  renderContent() {
-    const { content } = this.props;
-
-    if (content) return content;
-
-    const zoom = 12;
-    const center = [-104.9848, 39.7392];
-    const maxBounds = [[-105.27, 39.35], [-104.60, 39.93]];
-
-    return (
-      <MapGL
-        zoom={zoom}
-        center={center}
-        maxBounds={maxBounds}
-      />
-    );
   }
 
   render() {
     const { styles } = this;
 
-
     return (
       <div style={styles.root}>
-        <Panel>{this.props.panel || <IndexPanel />}</Panel>
-        <div style={styles.content}>{this.renderContent()}</div>
+        {this.props.children}
       </div>
     );
   }
--- src/common/routes/index.js
+++ src/common/routes/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { Router, Route, Redirect, browserHistory } from 'react-router';
-import { App, SearchResultsPanel } from '../containers';
+import { App, Home, SearchResultsPanel } from '../containers';
 
 export default (
   <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <Route path="search/q/:query" components={{ panel: SearchResultsPanel }} />
-      <Redirect from="*" to="/" />
+    <Route component={App}>
+      <Route path="/" component={Home}>
+        <Route path="search/q/:query" components={{ panel: SearchResultsPanel }} />
+        <Redirect from="*" to="/" />
+      </Route>
     </Route>
   </Router>
 );
