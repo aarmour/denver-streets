3d23d2ab79f59f52c9b06e70e2ead63a42e74062
Adam Armour <adam.armour@teamaol.com>
2016-02-14 19:14:37 -0700

    style: add Font Awesome

--- src/server/views/index-handler.js
+++ src/server/views/index-handler.js
@@ -34,6 +34,7 @@ function renderFullPage(html, data) {
     <html>
       <head>
         <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
+        <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
 
         <style>
           ${styles}
