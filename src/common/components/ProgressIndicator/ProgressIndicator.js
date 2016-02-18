5f92abba920657d375b9f32cc221424a7c8d77c5
Adam Armour <adam.armour@teamaol.com>
2016-02-17 21:24:41 -0700

    feat(progress indicator): add new component

---
+++ src/common/components/ProgressIndicator/ProgressIndicator.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function ProgressIndicator() {
+  return (
+    <div className="progress-indicator">
+      <div className="progress-indicator__bounce1"></div>
+      <div className="progress-indicator__bounce2"></div>
+      <div className="progress-indicator__bounce3"></div>
+    </div>
+  );
+}
--- src/common/components/index.js
+++ src/common/components/index.js
@@ -1,3 +1,5 @@
 export { default as MapGL } from './MapGL';
+export { default as Pagination } from './Pagination';
 export { default as Panel } from './Panel';
+export { default as ProgressIndicator } from './ProgressIndicator';
 export { default as SearchBar } from './SearchBar';
